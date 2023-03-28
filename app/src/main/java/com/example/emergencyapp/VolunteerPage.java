eefe98b613f94b33c3202cabfae329709363d12a
AhmedGH <ahmed.g@live.com>
2023-03-28 15:00:09 +0300

    new

---
+++ app/src/main/java/com/example/emergencyapp/VolunteerPage.java
@@ -0,0 +1,14 @@
+package com.example.emergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VolunteerPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_page);
+    }
+}
